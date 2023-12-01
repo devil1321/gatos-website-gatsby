e2e59b0001e25d8398cdeaf84722aec35b0eb622
devil1321 <s.dominik1321@gmail.com>
2023-12-01 14:22:06 +0100

    init become-partner and admin pages and components and pages styles

--- src/pages/become-partner.tsx
+++ src/pages/become-partner.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { GlobalComponents } from '../components/global'
+import { BecomePartnerComponents } from '../components/become-partner'
 import * as styles from '../styles/pages/become-partner.module.scss'
 
 const BecomePartner = () => {
   return (
     <GlobalComponents.Layout className={styles.become_partner}>
-      <div></div>
+      <BecomePartnerComponents.Hero />
+      <BecomePartnerComponents.Feature />
+      <BecomePartnerComponents.BussinesAndBranding />
+      <BecomePartnerComponents.StepComponents.Steps />
+      <BecomePartnerComponents.CustomersComponents.Customers />
+      <BecomePartnerComponents.MarketComponents.Market />
+      <BecomePartnerComponents.LocalBusinessComponents.LocalBusiness />
     </GlobalComponents.Layout>
   )
 }
